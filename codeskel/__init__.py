1f12e2578c0677629fbfc8bc8879cbd6cbac5918
erral <mlarreategi@codesyntax.com>
2008-06-20 10:22:48 +0000

    initial import
    svn path=/CodeSkel/trunk/; revision=12329

--- codeskel/__init__.py
+++ codeskel/__init__.py
@@ -1,6 +1,1 @@
-# package
-from codeskel.cs_plone25_theme import Plone25Theme
-from codeskel.cs_plone3_theme import Plone3Theme
-from codeskel.cs_plone3_buildout import Plone3Buildout
-from codeskel.udalplone_buildout import UdalPloneBuildout
-from codeskel.udalplone3_buildout import UdalPlone3Buildout
+#
--- setup.py
+++ setup.py
@@ -1,40 +1,26 @@
 from setuptools import setup, find_packages
-import codeskel
+import sys, os
 
-version = '0.5'
+version = '0.1'
 
 setup(name='CodeSkel',
       version=version,
-      description="A collection of skeletons for quickstarting CS projects.",
-      long_description=open('README.txt').read() + "\n" + 
-                       open('HISTORY.txt').read(),
-      classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Zope2",
-        "Framework :: Zope3",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='web zope command-line skeleton project',
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
       author='Mikel Larreategi',
       author_email='mlarreategi@codesyntax.com',
-      url='',
-      packages=find_packages(exclude=['ez_setup']),
+      url='http://code.codesyntax.com/private/CodeSkel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        "ZopeSkel",
+          # -*- Extra requirements: -*-
       ],
       entry_points="""
-      [paste.paster_create_template]
-      cs_plone25_theme = codeskel:Plone25Theme
-      cs_plone3_theme = codeskel:Plone3Theme
-      cs_plone3_buildout = codeskel:Plone3Buildout
-      udalplone_buildout = codeskel:UdalPloneBuildout
-      udalplone3_buildout = codeskel:UdalPlone3Buildout
+      # -*- Entry points: -*-
       """,
       )
