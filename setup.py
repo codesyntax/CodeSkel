45e013674a297f7209a23e3f8dc008845bd51fe2
erral <mlarreategi@codesyntax.com>
2008-06-20 10:31:16 +0000

    import
    svn path=/CodeSkel/trunk/; revision=12330

--- setup.py
+++ setup.py
@@ -5,11 +5,21 @@ version = '0.1'
 
 setup(name='CodeSkel',
       version=version,
-      description="",
+      description="A collection of skeletons for quickstarting CS projects.",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ], 
+      keywords='web zope command-line skeleton project',
       author='Mikel Larreategi',
       author_email='mlarreategi@codesyntax.com',
       url='http://code.codesyntax.com/private/CodeSkel',
@@ -18,9 +28,14 @@ setup(name='CodeSkel',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+         "ZopeSkel",
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [paste.paster_create_template]
+      cs_plone25_theme = codeskel:Plone25Theme
+      cs_plone3_theme = codeskel:Plone3Theme
+      cs_plone3_buildout = codeskel:Plone3Buildout
+      udalplone_buildout = codeskel:UdalPloneBuildout
+      udalplone3_buildout = codeskel:UdalPlone3Buildout
       """,
       )
