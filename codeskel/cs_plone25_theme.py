1526bdc522232d6d53d7e96b7678910358b244ff
erral <mlarreategi@codesyntax.com>
2010-07-06 08:28:08 +0000

    remove unneeded
    svn path=/CodeSkel/trunk/; revision=34471

--- codeskel/cs_plone25_theme.py
+++
@@ -1,5 +0,0 @@
-from zopeskel import Plone25Theme as ZSPlone25Theme
-
-class Plone25Theme(ZSPlone25Theme):
-    _template_dir = 'templates/plone2.5_theme'
-    summary = 'A theme for Plone 2.5 with proper quickinstaller skin installation'
--- codeskel/udalplone_buildout.py
+++
@@ -1,5 +0,0 @@
-from codeskel.cs_plone3_buildout import Plone3Buildout
-
-class UdalPloneBuildout(Plone3Buildout):
-    _template_dir = 'templates/udalplone_buildout'
-    summary = 'A simple buildout for UdalPlone (Plone 2.5.x)'
