b9ac1761fde01fc08895ee40a0c0a1c22ac0844d
erral <mlarreategi@codesyntax.com>
2012-03-08 11:25:53 +0000

    mark sub-temnpltes
    svn path=/CodeSkel/trunk/; revision=55992

--- codeskel/localcommands/dexterity.py
+++ codeskel/localcommands/dexterity.py
@@ -1,4 +1,6 @@
 from zopeskel.dexterity.localcommands.dexterity import DexterityContent
 
 class DexterityCS(DexterityContent):
+    parent_templates = ['dexterity', 'cs_dexterity']
+                        
     _template_dir = 'templates/dexterity/content_cs'
