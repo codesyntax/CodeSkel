6af9089ba5dc56b3adeb275f44ec80cb69e27874
erral <mlarreategi@codesyntax.com>
2012-03-08 11:34:50 +0000

    commint
    svn path=/CodeSkel/trunk/; revision=55994

--- codeskel/localcommands/dexterity.py
+++ codeskel/localcommands/dexterity.py
@@ -4,3 +4,4 @@ class DexterityCS(DexterityContent):
     parent_templates = ['dexterity', 'cs_dexterity']
                         
     _template_dir = 'templates/dexterity/content_cs'
+    summary = 'A content type skeleton for Dexterity based on CS conventions'
