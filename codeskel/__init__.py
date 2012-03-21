8679940a159f986f1c1ca960dd4e269fa0a057b5
erral <mlarreategi@codesyntax.com>
2012-03-21 15:12:46 +0000

    remove Plone 3 legacy templates
    svn path=/CodeSkel/trunk/; revision=56446

--- codeskel/__init__.py
+++ codeskel/__init__.py
@@ -1,8 +1,6 @@
 #
-from cs_plone3_buildout import Plone3Buildout
 from cs_plone4_buildout import Plone4Buildout
 from cs_plone3_theme import Plone3Theme
-from udalplone3_buildout import UdalPlone3Buildout
 from dexterity_cs import CSDexterity
 from localcommands.dexterity import DexterityContentCS
 
--- codeskel/udalplone3_buildout.py
+++
@@ -1,5 +0,0 @@
-from codeskel.cs_plone3_buildout import Plone3Buildout
-
-class UdalPlone3Buildout(Plone3Buildout):
-    _template_dir = 'templates/udalplone3_buildout'
-    summary = 'A simple buildout for UdalPlone (Plone 3.x)'
