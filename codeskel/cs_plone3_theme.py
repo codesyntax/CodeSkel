90fb4f3eb6e31794fdf4949a4567f4a03313232c
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-02-20 11:36:58 +0100

    generate a default human readable description

--- codeskel/cs_plone3_theme.py
+++ codeskel/cs_plone3_theme.py
@@ -2,6 +2,7 @@ from zopeskel import Plone3Theme as ZSPlone3Theme
 from zopeskel.base import get_var
 import copy
 
+
 class Plone3Theme(ZSPlone3Theme):
     _template_dir = 'templates/plone3_theme'
     summary = 'A Theme for Plone 3/4 with no browser resources'
@@ -13,7 +14,7 @@ class Plone3Theme(ZSPlone3Theme):
     get_var(vars, 'version').default = '1.0'
     get_var(vars, 'version').modes = ()
 
-    get_var(vars, 'description').default = 'Installable theme: %s.%s' % (get_var(vars, 'namespace_package'), get_var(vars, 'package'))
+    get_var(vars, 'description').default = 'Installable theme: %s.%s' % (str(get_var(vars, 'namespace_package')), str(get_var(vars, 'package')))
     get_var(vars, 'description').modes = ()
 
     get_var(vars, 'empty_styles').default = True
@@ -22,6 +23,11 @@ class Plone3Theme(ZSPlone3Theme):
     get_var(vars, 'include_doc').default = True
     get_var(vars, 'include_doc').modes = ()
 
+    get_var(vars, 'skinname').default = '(CHANGE THIS) Custom Theme'
+
+    def pre(self, command, output_dir, vars):
+        super(Plone3Theme, self).pre(command, output_dir, vars)
+        vars['description'] = 'Installable theme: %s.%s' % (vars['namespace_package'], vars['package'])
 
     def post(self, command, output_dir, vars):
         pass
