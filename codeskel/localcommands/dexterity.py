d693de1acc075139165eb461924bf9ef2c3bb587
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-03-07 09:21:39 +0100

    create contenttype names based on the content-type not the full package

--- codeskel/localcommands/dexterity.py
+++ codeskel/localcommands/dexterity.py
@@ -1,12 +1,12 @@
 from zopeskel.dexterity.localcommands.dexterity import DexterityContent
 
+
 class DexterityContentCS(DexterityContent):
     parent_templates = ['dexterity', 'csdexterity']
-                       
+
     _template_dir = 'templates/dexterity/content_cs'
     summary = 'A content type skeleton for Dexterity based on CS conventions'
 
     def pre(self, command, output_dir, vars):
         super(DexterityContentCS, self).pre(command, output_dir, vars)
         vars['add_permission_id'] = vars['package_dotted_name'] + '.Add' + vars['contenttype_name']
-        
