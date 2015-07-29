597e08209bc6b2df8e2fafe2a33a2b3921f001d5
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-07-29 20:20:05 +0200

    initial attempt to create a plone5 compatible theme

---
+++ codeskel/cs_plone5_theme.py
@@ -0,0 +1,33 @@
+from zopeskel import Plone3Theme as ZSPlone3Theme
+from zopeskel.base import get_var
+import copy
+
+
+class Plone5Theme(ZSPlone3Theme):
+    _template_dir = 'templates/cs_plone5_theme'
+    summary = 'A Theme for Plone 5 using Diazo and Barceloneta'
+    skinbase = 'Default'
+    use_local_commands = True
+
+    vars = copy.deepcopy(ZSPlone3Theme.vars)
+
+    get_var(vars, 'version').default = '1.0'
+    get_var(vars, 'version').modes = ()
+
+    get_var(vars, 'description').default = 'Installable theme'
+    get_var(vars, 'description').modes = ()
+
+    get_var(vars, 'empty_styles').default = True
+    get_var(vars, 'empty_styles').modes = ()
+
+    get_var(vars, 'include_doc').default = True
+    get_var(vars, 'include_doc').modes = ()
+
+    get_var(vars, 'skinname').default = '(CHANGE THIS) Custom Theme'
+
+    def pre(self, command, output_dir, vars):
+        super(Plone3Theme, self).pre(command, output_dir, vars)
+        vars['description'] = 'Installable theme: %s.%s' % (vars['namespace_package'], vars['package'])
+
+    def post(self, command, output_dir, vars):
+        pass
