584407748de9cdeeff5d0d91892d1011a31f104b
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-03-16 16:51:05 +0100

    remove grok traces from dexterity template

--- codeskel/dexterity_cs.py
+++ codeskel/dexterity_cs.py
@@ -1,4 +1,8 @@
 from zopeskel.dexterity.dexterity import Dexterity
+from zopeskel.plone import BasicZope
+
 
 class CSDexterity(Dexterity):
     _template_dir = 'templates/dexterity_cs'
+
+    vars = BasicZope.vars
