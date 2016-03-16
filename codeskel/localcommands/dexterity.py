618c1bfbab6c0426c696d04477a85889c4b480b8
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-03-16 17:46:53 +0100

    only ask needed questions

--- codeskel/localcommands/dexterity.py
+++ codeskel/localcommands/dexterity.py
@@ -1,4 +1,5 @@
 from zopeskel.dexterity.localcommands.dexterity import DexterityContent
+from zopeskel.base import var
 
 
 class DexterityContentCS(DexterityContent):
@@ -7,6 +8,15 @@ class DexterityContentCS(DexterityContent):
     _template_dir = 'templates/dexterity/content_cs'
     summary = 'A content type skeleton for Dexterity based on CS conventions'
 
+    vars = [
+        var('contenttype_name', 'Content type name ', default='Example Type'),
+        var('folderish', 'True/False: Content type should act as a container ',
+            default=False),
+    ]
+
     def pre(self, command, output_dir, vars):
         super(DexterityContentCS, self).pre(command, output_dir, vars)
         vars['add_permission_id'] = vars['package_dotted_name'] + '.Add' + vars['contenttype_name']
+        vars['global_allow'] = True
+        vars['allow_discussion'] = False
+        vars['contenttype_description'] = 'Description of this type'
