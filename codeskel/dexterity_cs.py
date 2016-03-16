d9a11873394d664bab13e43826392ddbafdcaf8f
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-03-16 17:46:32 +0100

    improve description

--- codeskel/dexterity_cs.py
+++ codeskel/dexterity_cs.py
@@ -4,5 +4,5 @@ from zopeskel.plone import BasicZope
 
 class CSDexterity(Dexterity):
     _template_dir = 'templates/dexterity_cs'
-
+    summary = "A Dexterity-based product following CS conventions"
     vars = BasicZope.vars
